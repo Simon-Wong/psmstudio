c1ce1472cd43565d07203c84424f9ce3cc038cda
thbytwo <thbytwo@live.cn>
2026-04-17 13:48:20 +0800

    fix something

--- service/sound/wrapper/index_tts/api_index_tts.py
+++ service/sound/wrapper/index_tts/api_index_tts.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
     path2 = generate_tts(arg_prompt=r"D:\testGit\psmstudio\service\sound\wrapper\index_tts\demo_voice.mp3",
                          arg_text="溪柴火软蛮毡暖，我与狸奴不出门。",
                          arg_emo_ref_path=r"D:\testGit\psmstudio\service\sound\wrapper\index_tts\demo_voice.mp3",
-                         arg_output_path=r"D:\testGit\psmstudio\temp",
+                         arg_output_path=r"D:\testGit\psmstudio",
                          arg_output_filename="hahagushi_2",
                          arg_verbose=True,
                          emo_weight=0.7,
